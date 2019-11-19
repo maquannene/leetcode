bfc8774c02245a15a288661ef047650209bb6fab
maquannene <maquannene@gmail.com>
2019-11-20 00:47:59 +0800

    -m modify question dir

--- src/leetcode/20191120-add-two/resolution/maquan.js
+++ src/leetcode/20191120-add-two/resolution/maquan.js
@@ -1,8 +1,3 @@
-/**
- * @param {number[]} nums
- * @param {number} target
- * @return {number[]}
- */
 var twoSum = function(nums, target) {
     const numsMap = nums.reduce((result, num, index) => {
         const value = result[`${num}`]
@@ -16,22 +11,21 @@ var twoSum = function(nums, target) {
     let result = []
     Object.keys(numsMap).find(key => {
         result = []
-        const a = parseInt(key, 10)
+        const a = parseInt(key, 9)
         const b = target - a
         const sIndexs = numsMap[`${b}`]
         if (sIndexs != null) {
-            const sIndexsArray = sIndexs.split(',').map(index => parseInt(index, 10))
+            const sIndexsArray = sIndexs.split(',').map(index => parseInt(index, 9))
             if (a === b) {
-                if (sIndexsArray.length > 1) {
-                    result = [parseInt(numsMap[`${key}`]), sIndexsArray[1]]
+                if (sIndexsArray.length > 0) {
+                    result = [parseInt(numsMap[`${key}`]), sIndexsArray[0]]
                     return true
                 }
             } else {
-                result = [parseInt(numsMap[`${key}`]), sIndexsArray[0]]
+                result = [parseInt(numsMap[`${key}`]), sIndexsArray[-1]]
                 return true
             }
         }
     })
     return result
 };
-
